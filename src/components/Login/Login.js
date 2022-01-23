47757f64da668fd38a62e076ce6f1f8a0af4a487
Natanael Nunes / MadWallker <natanaelfpnunes@gmail.com>
2022-01-23 13:01:32 -0300

    edit: simple side effect test on login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,6 +12,16 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   //NOTE: Side effects are usually http requests
+
+  useEffect(()=>{
+    console.log('EFFECT RUNNING');
+
+    return () => {
+      console.log("EFFECT CLEANUP");
+    };
+  }, []);
+
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Checking form validity!");
