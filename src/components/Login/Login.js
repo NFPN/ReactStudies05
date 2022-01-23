bdc1e7708919be00f068b552c24d92b82cdc974d
Natanael Nunes / MadWallker <natanaelfpnunes@gmail.com>
2022-01-23 12:54:59 -0300

    edit: use debouncing to login validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,9 +13,17 @@ const Login = (props) => {
 
   //NOTE: Side effects are usually http requests
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity!");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 700);
+
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
